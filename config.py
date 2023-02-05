d19abca3ec79094cdeff72b2343a39b6899b90e2
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-02-05 07:37:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     OWNER_ID = os.environ.get("OWNER_ID", 12345)
     LIMIT = int(os.environ.get("LIMIT", "25000"))
     SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
-    SESSION = os.environ.get("SESSION")
+    SESSION = os.environ.get("SESSION", "BQAlMzYWr3AFQgzppOcTWe3Mh7VGCKGAnObs6tykVwyexB04GNElsS1o12aRSeTTuGamgNJMssiUls33QEUfTajt27zrUQCi0HQ-qCCI8D_IT6pDnlv2tgCnHwkcEYJ6vUgBxPZSdMkkfnQ4tiL93MA4Vxt2Gwd6FUrxD2qwUXOQRnXUFIBTYPi1ihcjkijBU3-iixEsQwOYISLuM3sJ10liG-CdITfVSAhdIYciegR4-BNIHGmeYs38GMbbhFA0dM0i0z7Orm7FsziOZ_r56YbWbWuOH8R3Re-7hntBrThvXP_nI47zmYxuC8zIZ-qleZiI-VhHRoBCHNA46VWf6xHlUvF4PAA")
     TO_CHANNEL = os.environ.get("TO_CHANNEL", "")
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
